ffb5a73c27672887b53f9f424303db4ea47db091
sjimmie bleeker <blee1703@student.nhl.nl>
2018-09-18 12:30:04 +0200

    test
    test

--- AmazonSimulator VS/Program.cs
+++ AmazonSimulator VS/Program.cs
@@ -7,7 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-//TestJan
+//Testsjimsjum
+
 namespace AmazonSimulator_VS
 {
     public class Program
