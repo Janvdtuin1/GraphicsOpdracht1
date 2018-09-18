46d137fdd9a7bdc2c82fd6fa421a3fc77436a41e
Janvdtuin1 <32636731+Janvdtuin1@users.noreply.github.com>
2018-09-18 12:27:53 +0200

    Jancommit

--- AmazonSimulator VS/Program.cs
+++ AmazonSimulator VS/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-//test
+//TestJan
 namespace AmazonSimulator_VS
 {
     public class Program
