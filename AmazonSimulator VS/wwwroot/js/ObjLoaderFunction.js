70b1b7570b6a51f6250602fe0adc9e8e028d9144
Janvdtuin1 <32636731+Janvdtuin1@users.noreply.github.com>
2018-10-07 21:22:52 +0200

    Merge

---
+++ AmazonSimulator VS/wwwroot/js/ObjLoaderFunction.js
@@ -0,0 +1,30 @@
+﻿/**
+ * 
+ * @param {string} modelPath Het pad naar het model op de server
+ * @param {string} modelName De naam van het model in de map (OBJ bestand)
+ * @param {string} texturePath Het pad naar de texture van het model
+ * @param {string} textureName De naam van het texturebestand (MTL)
+ * @param {function(THREE.Mesh): void} onload Die functie die aangeroepen wordt als het model geladen is
+ * @return {void}
+
+ */
+
+function LoadOBJModel(modelPath, modelName, texturePath, textureName, onload) {
+    new THREE.MTLLoader()
+        .setPath(texturePath)
+        .load(textureName, function (materials) {
+            materials.preload();
+
+            new THREE.OBJLoader()
+                .setPath(modelPath)
+                .setMaterials(materials)
+                .load(modelName, function (object) {
+                    onload(object);
+
+
+                }, function () { }, function (e) { console.log("Er is iets mis gegaan tijdens het laden van het model"); console.log(e); });
+
+        });
+
+        }
+
---
+++ AmazonSimulator VS/wwwroot/js/Weg.js
@@ -0,0 +1,29 @@
+﻿function WegEnStoep() {
+    var weggroup = new THREE.Group();
+    //weg met stoep-
+    var weggeometry = new THREE.PlaneGeometry(100, 10, 32);
+
+    var wegmateriaal = new THREE.MeshStandardMaterial({ map: new THREE.TextureLoader().load("textures/weg/weg.jpg"), side: THREE.DoubleSide });
+    var wegplane = new THREE.Mesh(weggeometry, wegmateriaal);
+    wegplane.rotation.x = Math.PI / 2.0;
+    wegplane.position.x = 15;
+    wegplane.position.z = 35;
+
+    weggroup.add(wegplane);
+    var stoepgeometry = new THREE.PlaneGeometry(100, 5, 32);
+    var stoeptexture = new THREE.TextureLoader().load("textures/weg/stonebrick.png");
+    stoeptexture.wrapS = THREE.RepeatWrapping;
+    stoeptexture.wrapT = THREE.RepeatWrapping;
+    stoeptexture.repeat.set(16,2);
+
+
+    var stoepmateriaal = new THREE.MeshStandardMaterial({ map: stoeptexture, side: THREE.DoubleSide});
+
+    var stoepplane = new THREE.Mesh(stoepgeometry, stoepmateriaal);
+    stoepplane.rotation.x = Math.PI / 2.0;
+    stoepplane.position.x = 15;
+    stoepplane.position.z = 42.5;
+    weggroup.add(stoepplane);
+    return weggroup;
+    
+}
